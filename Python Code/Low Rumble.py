3c32941389f27d0a284bfcbb049a2e6e8e39f8d6
Tristan Protzman <tlprotzman+github@gmail.com>
2016-10-01 14:09:31 -0400

    More code

--- Python Code/Low Rumble.py
+++ Python Code/Low Rumble.py
@@ -1,19 +1,21 @@
 from psonic import *
+rest = 0.126
+
+while True:
+	for i in range(16):
+		play(C3)
+		sleep(rest)
+
+	for i in range(16):
+		play(A2)
+		sleep(rest)
+
+	for i in range(16):
+		play(E3)
+		sleep(rest)
 
 
-def createList(length):
-	num = [1, 1]
-	while len(num) < length:
-		index = len(num)
-		print(len(num))
-		num.append(num[index - 1] + num[index - 2])
-	return num
-
-
-notes = createList(12)
-print(notes)
-
-for i in notes:
-	play(i)
-	print(i)
-	sleep(1)
+	for i in range(16):
+		play(D3)
+		sleep(rest)
+	
